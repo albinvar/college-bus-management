51f66bb8de0402667664e8263a9ea4ca93fafaf4
Albin Varghese <albin8285@gmail.com>
2024-05-01 20:04:03 +0530

    fix: responsive actions buttons

--- web/resources/views/roles/admin/manage-bus-students.blade.php
+++ web/resources/views/roles/admin/manage-bus-students.blade.php
@@ -261,7 +261,7 @@
                                                 </td>
 
                                                 <td class="size-px whitespace-nowrap">
-                                                    <div class="px-6 py-1.5">
+                                                    <div class="px-6 py-1.5 flex space-x-2">
                                                         <button data-hs-overlay="#hs-notifications-{{ $student->id }}" class="py-1.5 px-2 inline-flex items-center gap-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
                                                             <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"/><path d="M12 8v4l2 2"/><path d="M12 16h.01"/></svg>
                                                             Link Card
